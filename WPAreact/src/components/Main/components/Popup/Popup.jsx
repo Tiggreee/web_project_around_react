c3d0eef586686d65cb9cc44cc818d9fe8e740c8e
Tigresito <tiggreee@outlook.com>
2025-09-24 08:00:31 -0600

    feat: Agrega imágenes correctas de close button del Figma con efecto hover

--- WPAreact/src/components/Main/components/Popup/Popup.jsx
+++ WPAreact/src/components/Main/components/Popup/Popup.jsx
@@ -43,7 +43,7 @@ export default function Popup(props) {
           type="button"
           onClick={onClose}
         >
-          ×
+          <img src={closeIcon} alt="Cerrar" />
         </button>
         
         {title && <h3 className="popup__title">{title}</h3>}
